799d1a538a9b9b495bf5f7dd5ca302558d7e3e38
Pui Chan <u1725448@live.warwick.ac.uk>
2019-11-04 16:34:49 +0000

    Applied changes to Linux version, added jumping to input interface

--- GameController.py
+++ GameController.py
@@ -1,60 +1,90 @@
 from pynput.keyboard import Key, Controller
 import time
+import threading
 
 kb = Controller()
-previnput = list()
+previnput = set()
 
-def release():
-    print(previnput)
-    if "a" in previnput:
-        kb.release("a")
-        previnput.remove("a")
-    if "s" in previnput:
-        kb.release("s")
-        previnput.remove("s")
-    if "d" in previnput:
-        kb.release("d")
-        previnput.remove("d")
-    if "f" in previnput:
-        kb.release("f")
-        previnput.remove("f")
+''' Movement controls (ASDF) '''
+def movement(func):
+    def releaseAndPress():
+        if "a" in previnput:
+            kb.release("a")
+            previnput.remove("a")
+        if "s" in previnput:
+            kb.release("s")
+            previnput.remove("s")
+        if "d" in previnput:
+            kb.release("d")
+            previnput.remove("d")
+        if "f" in previnput:
+            kb.release("f")
+            previnput.remove("f")
+        inputs = func()
+        previnput.clear()
+        for i in range(len(inputs)):
+            previnput.add(inputs[i])
+    return releaseAndPress
 
+@movement
 def walkLeft():
-    release()
     kb.press("a")
+    return ("a")
 
+@movement
 def runLeft():
-    release()
     kb.press("f")
+    time.sleep(0.03)
     kb.press("a")
+    return ("a","f")
 
+@movement
 def walkRight():
-    release()
     kb.press("d")
+    return ("d")
 
+@movement
 def runRight():
-    release()
     kb.press("f")
+    time.sleep(0.03)
     kb.press("d")
+    return ("d","f")
 
+@movement
 def crouch():
-    release()
     kb.press("s")
+    return ("s")
 
+@movement
 def stand():
-    release()
-    return
+    return ()
 
+
+''' Jump controls '''
+def jumpLow():
+    kb.press("e")
+    time.sleep(0.05)
+    kb.release("e")
+
+def jumpHigh():
+    kb.press("e")
+    time.sleep(0.2)
+    kb.release("e")
+
+
+def jumpTest():
+    for i in range(5):
+        jumpLow()
+        time.sleep(0.2)
+    
 if __name__ == "__main__":
+    jmpthread = threading.Thread(target=jumpTest)
     time.sleep(3)
-    walkLeft()
-    time.sleep(1)
-    walkRight()
-    time.sleep(1)
-    runLeft()
-    time.sleep(1)
-    runRight()
-    time.sleep(1)
-    crouch()
-    time.sleep(1)
+    jmpthread.start()
+    #walkLeft()
+    #time.sleep(2)
+    #walkRight()
+    #time.sleep(2)
     stand()
+    jmpthread.join()
+    
