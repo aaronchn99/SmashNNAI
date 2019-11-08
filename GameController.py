37b27430ad14eaef81e70e2ae85b3174a92e6075
Pui Chan <u1725448@live.warwick.ac.uk>
2019-11-08 20:31:00 +0000

    Tried scripting controller frames 	- Easier to code, but may take longer for AI to adapt to controls

--- GameController.py
+++ GameController.py
@@ -4,6 +4,8 @@ import threading
 
 kb = Controller()
 previnput = set()
+SHORT = 0.05
+LONG = 0.2
 
 ''' Movement controls (ASDF) '''
 def movement(func):
@@ -63,28 +65,38 @@ def stand():
 ''' Jump controls '''
 def jumpLow():
     kb.press("e")
-    time.sleep(0.05)
+    time.sleep(SHORT)
     kb.release("e")
 
 def jumpHigh():
     kb.press("e")
-    time.sleep(0.2)
+    time.sleep(LONG)
     kb.release("e")
 
 
+''' A Attack controls '''
+@movement
+def neutralA():
+    kb.press("p")
+    time.sleep(SHORT)
+    kb.release("p")
+    return ()
+
 def jumpTest():
     for i in range(5):
-        jumpLow()
+        jumpHigh()
         time.sleep(0.2)
-    
+
 if __name__ == "__main__":
     jmpthread = threading.Thread(target=jumpTest)
     time.sleep(3)
     jmpthread.start()
-    #walkLeft()
-    #time.sleep(2)
-    #walkRight()
-    #time.sleep(2)
+    for i in range(10):
+        neutralA()
+        time.sleep(0.1)
+    walkLeft()
+    time.sleep(2)
+    walkRight()
+    time.sleep(2)
     stand()
     jmpthread.join()
-    
