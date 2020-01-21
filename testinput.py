4ec79050d775111a89069bb2345d257b9a05a356
aaronchn99 <P.Chan.15@warwick.ac.uk>
2020-01-21 17:37:54 +0000

    Completed input test script

--- testinput.py
+++ testinput.py
@@ -2,6 +2,7 @@ import BasicController as bc
 import json
 import time
 
+
 sequences = dict()
 
 def runSequence(seq):
@@ -11,7 +12,25 @@ def runSequence(seq):
 with open("testing/test_sequences.json", "r") as tf:
     sequences = json.load(tf)
 
-time.sleep(2)
-for seq in sequences["sequence"].keys():
-    runSequence(sequences["sequence"][seq])
+k = ""
+
+while k != "q":
     runSequence(sequences["sequence"]["stand"])
+    if k == "1":
+        runSequence(sequences["grab"])
+    elif k == "2":
+        runSequence(sequences["grab_a"])
+    elif k == "3":
+        runSequence(sequences["grab_left"])
+    elif k == "4":
+        runSequence(sequences["grab_right"])
+    elif k == "5":
+        runSequence(sequences["grab_up"])
+    elif k == "6":
+        runSequence(sequences["grab_down"])
+    elif k == "s":
+        for seq in sequences["sequence"].keys():
+            runSequence(sequences["sequence"][seq])
+            runSequence(sequences["sequence"]["stand"])
+    runSequence(sequences["sequence"]["stand"])
+    k = input("1-grab, 2-grab_a, 3-grab_l, 4-grab_r, 5-grab_u, 6-grab_d, s-sequence, q-quit:  ")
